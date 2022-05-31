e591bd004ed2679056bf96d7d4f57a174040e258
NormanR20 <norman17.rm@gmail.com>
2022-05-31 10:10:05 -0600

    Updates

---
+++ precache-manifest.a23502914aaf65c872f5bebc688c70f7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8b5eb31992980fedf4c",
+    "url": "/static/css/main.eb2ba5ee.chunk.css"
+  },
+  {
+    "revision": "a8b5eb31992980fedf4c",
+    "url": "/static/js/main.a8b5eb31.chunk.js"
+  },
+  {
+    "revision": "eb6aaad9a165f757823b",
+    "url": "/static/css/1.732a9e60.chunk.css"
+  },
+  {
+    "revision": "eb6aaad9a165f757823b",
+    "url": "/static/js/1.eb6aaad9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a76eb1f3381a1198eb351d92ba16eff8",
+    "url": "/static/media/htmlIcon.a76eb1f3.png"
+  },
+  {
+    "revision": "9541a9362afb148a68dd2b02272690df",
+    "url": "/static/media/gamer-4k-game-console-tv-night.9541a936.jpg"
+  },
+  {
+    "revision": "f926e0040fc2663c3ea45ecd7ef504cf",
+    "url": "/static/media/mysqlIcon.f926e004.png"
+  },
+  {
+    "revision": "f2d76ecbfe97ebb034cbe393dd36d551",
+    "url": "/static/media/mongodbIcon.f2d76ecb.png"
+  },
+  {
+    "revision": "bf4c694a5cc217735174e5f64a8e0004",
+    "url": "/static/media/react.bf4c694a.png"
+  },
+  {
+    "revision": "82aa8963de722c8333f6349cf53ac317",
+    "url": "/static/media/cmsOneClick.82aa8963.svg"
+  },
+  {
+    "revision": "15e81b1f05cf0a981449476339232a9f",
+    "url": "/static/media/PoaPacc.15e81b1f.svg"
+  },
+  {
+    "revision": "adf79099f3333b9eb2d3924a2882ba69",
+    "url": "/static/media/rrhh_aurora.adf79099.svg"
+  },
+  {
+    "revision": "d883c38807089a496e81dc34e951e663",
+    "url": "/static/media/InstagramIcon.d883c388.png"
+  },
+  {
+    "revision": "744bf3b8925a29fb30a6f5c05165f78c",
+    "url": "/static/media/computadora-portatil.744bf3b8.svg"
+  },
+  {
+    "revision": "37f06308332b64a8bccad0003fde2986",
+    "url": "/static/media/IMG_1946.37f06308.JPG"
+  },
+  {
+    "revision": "43111572bda90fe1adb3e38a363efd84",
+    "url": "/static/media/HojaDeVida-NormanRaudales.43111572.pdf"
+  },
+  {
+    "revision": "8dfd0d8cbcd6d373e52f63f7cd116d71",
+    "url": "/static/media/Resume-NormanRaudales.8dfd0d8c.pdf"
+  },
+  {
+    "revision": "f6756e3075c8c45ef95f8496ac98680b",
+    "url": "/index.html"
+  }
+];
